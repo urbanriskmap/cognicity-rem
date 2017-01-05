f9ff7430126a7d9c3a21cf44d50dd9baa60f1977
mgvarley <b03d66df56994e4e0f2534fa616cdb06b97896e0>
2017-01-05 17:54:24 +0100

    Added dependencies, bootstrap and auth0 login

---
+++ assets/README.md
@@ -0,0 +1,29 @@
+**Folder structure and file types:**
+assets/graphics/*
+- Place vector graphics here, used as backgrounds to enhance UX.
+- Logos, stand-in graphics, water depth slider reference, etc.
+
+assets/icons/*
+- Map related content only.
+- Icons, markers
+
+assets/images/*
+- Raster image content for website pages.
+- Background images, photos for petabencana.id/about & petabencana.id/watch pages.
+
+
+**Leaflet custom control icons**
+- Create rectangle, 96px x 96px, no fill or stroke - as a transparent frame.
+- Place icon content within it.
+
+**Leaflet markers**
+- No frame required.
+- Restrict size to 48px x 48px.
+
+
+**fontello icons**
+- download font package from fontello.io
+- replace fonts, css folders in assets with the downloaded ones
+- list of current icons
+  icon-cancel-1, icon-target, icon-down-open, icon-left-open, icon-right-open, icon-camera, icon-cw, icon-trash-empty
+  ,icon-up-open, icon-picture, icon-facebook, icon-angle-double-right, icon-whatsapp, icon-twitter, icon-picture
--- src/app.js
+++ src/app.js
@@ -1,5 +1,63 @@
+import { I18N } from 'aurelia-i18n';
+import { HttpClient } from 'aurelia-fetch-client';
+import { tokenIsExpired } from './utils';
+
+// TODO: Move into environment variables
+const AUTH0_CLIENT_ID = '';
+const AUTH0_DOMAIN = '';
+
 export class App {
-  constructor() {
-    this.message = 'Hello World!';
+
+  lock = new Auth0Lock(AUTH0_CLIENT_ID, AUTH0_DOMAIN);
+  isAuthenticated = false;
+
+  static inject = [I18N, HttpClient];
+
+  constructor(i18n, http) {
+    this.i18n = i18n;
+    this.http = http;
+    var self = this;
+
+    if (tokenIsExpired()) {
+      this.isAuthenticated = false;
+    } else {
+      this.isAuthenticated = true;
+    }
+
+    this.lock.on("authenticated", (authResult) => {
+      self.lock.getProfile(authResult.idToken, (error, profile) => {
+        if (error) {
+          // Handle error
+          return;
+        }
+
+        localStorage.setItem('id_token', authResult.idToken);
+        localStorage.setItem('profile', JSON.stringify(profile));
+        self.isAuthenticated = true;
+        self.lock.hide();
+      });
+    });
+  }
+
+  configureRouter(config, router){
+    config.title = this.i18n.tr('title');
+    config.options.pushState = true;
+    config.options.root = '/';
+    config.map([
+      { route: '', moduleId: 'home', title: 'Home' },
+      { route: 'map', moduleId: 'map', title: 'Map', name: 'Map' }
+    ]);
+
+    this.router = router;
+  }
+
+  login() {
+    this.lock.show();
+  }
+
+  logout() {
+    localStorage.removeItem('profile');
+    localStorage.removeItem('id_token');
+    this.isAuthenticated = false;
   }
 }

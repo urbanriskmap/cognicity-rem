3f24d343db9c02a1b48c52c9ce960e55a7a6e813
Tomas <tomasholderness@gmail.com>
2017-02-15 11:14:40 -0500

    Fixed flood gauge graph bug

--- src/map.js
+++ src/map.js
@@ -304,7 +304,7 @@ export class Map {
   							pointBackgroundColor: "rgba(151,187,205,1)",
   							pointBorderColor: "#fff",
   	            pointRadius: 4,
-  							data: [1,2,3]
+  							data: []
   						}]
   					};
   					for (var i = 0; i < feature.properties.observations.length; i++){
